a076fb22bcd0870401f2d4e470c22fc3d0cdef4a
Matheus <morais.matheus.web@gmail.com>
2022-02-09 11:20:24 -0300

    chore: add env variables

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 
@@ -7,5 +10,5 @@ app.use(express.json());
 app.use(cors());
 
 app.listen(process.env.PORT, () => {
-  console.log("Server running on port " + process.env.PORT);
+  console.log("Server is listening on port " + process.env.PORT);
 });
